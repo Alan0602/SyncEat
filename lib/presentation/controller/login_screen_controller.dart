19853c921871678de16bb5da1d20deec73655fa3
alan.anil <alank.anil@coolmindsinc.com>
2025-06-27 13:05:56 +0530

    change the navigation

--- lib/presentation/controller/login_screen_controller.dart
+++ lib/presentation/controller/login_screen_controller.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:synceat/core/constants/color_constants.dart';
-import 'package:synceat/presentation/widgets/auth/login_screen.dart';
+import 'package:synceat/presentation/screens/auth/login_screen.dart';
 
 class LoginScreenController with ChangeNotifier {
   bool islogined = false;
